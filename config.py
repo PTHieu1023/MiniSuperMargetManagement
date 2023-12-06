066667fded2291ae5db834c49ea901fa58fe80c1
Pham Trung Hieu <hjuniik.music@gmail.com>
2023-12-06 14:45:46 +0700

    test config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+DATABASE = {
+	"NAME": "dellstore2",
+    "USER": "postgres",
+    "PASSWORD": "yeah1022003",
+    "HOST": "127.0.0.1",
+    "PORT": "10203",
+}
--- minisupermarketmanagement.py
+++ minisupermarketmanagement.py
@@ -3,11 +3,10 @@ import sys
 
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
-# Important:
-# You need to run the following command to generate the ui_form.py file
-#     pyside6-uic form.ui -o ui_form.py, or
-#     pyside2-uic form.ui -o ui_form.py
-from ui_form import Ui_MiniSuperMarketManagement
+from UI_Forms.ui_py.manager_screen import Ui_MiniSuperMarketManagement
+
+import psycopg2
+import config
 
 class MiniSuperMarketManagement(QMainWindow):
     def __init__(self, parent=None):
@@ -21,3 +20,45 @@ if __name__ == "__main__":
     widget = MiniSuperMarketManagement()
     widget.show()
     sys.exit(app.exec())
+
+
+
+
+
+
+
+    # conn_string = f"dbname={config.DATABASE['NAME']} user={config.DATABASE['USER']} password={config.DATABASE['PASSWORD']} host={config.DATABASE['HOST']} port={config.DATABASE['PORT']}"
+    #
+    # # Kết nối đến PostgreSQL
+    # try:
+    #     connection = psycopg2.connect(conn_string)
+    #     print("Kết nối thành công!")
+    #     cursor = connection.cursor()
+    #
+    #     # Chuỗi truy vấn SELECT
+    #     select_query = "SELECT * FROM public.orders"
+    #
+    #     # Thực hiện truy vấn
+    #     cursor.execute(select_query)
+    #
+    #     # Lấy kết quả truy vấn
+    #     records = cursor.fetchall()
+    #
+    #     # In kết quả
+    #     for row in records:
+    #         print(row)
+    #     column_names = [desc[0] for desc in cursor.description]
+    #
+    #     # Print column names
+    #     print("Columns:")
+    #     for col in column_names:
+    #         print(col)
+    #
+    # except Exception as e:
+    #     print(f"Lỗi kết nối: {e}")
+    #
+    # finally:
+    #     # Đóng kết nối sau khi sử dụng
+    #     if connection:
+    #         connection.close()
+    #         print("Kết nối đã đóng.")
